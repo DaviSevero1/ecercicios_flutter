373623125de0be44201351686533021f3cb7798c
Davi Severo Santana <severosantana2014@gmail.com>
2022-09-23 18:01:01 -0300

    Subindo atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ('Testando'),
+      title: ('Exercises_Tracking'),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
